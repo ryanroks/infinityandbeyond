ca10a4a6e0ed9a09e15df2db6f4fc919964962b5
ryanroks <ryanhargrove2@gmail.com>
2013-11-04 20:43:32 -0800

    First

---
+++ js/jquery.infinityandbeyond.js
@@ -0,0 +1,74 @@
+$.fn.infiniteScroll = function(options) {
+	
+	var results = [];
+	
+	var settings = $.extend(
+		{
+			auto: false,		
+			/***
+				True will auto load new data, 
+				False will require a click 
+			***/
+			buttonText: "Load More"
+		},
+		options);
+			
+	$(this).html("<div id='returnData'><img src=\"images/ajax-loader.gif\" style=\"margin-left:50%;\"></div>");
+	
+	if(settings.auto == false)
+		$(this).append("<div id='loadMore'>"+settings.buttonText+"</div>");
+	
+	$.getJSON("json/json.html",
+		function(data) {
+			$.each(data, function(index, value) {
+				results.push("<div class='row'>"+value.name+"</div>");
+				})
+			$("#returnData").html(results.join(''));
+			}
+		);
+
+	if(settings.auto == false) {
+		
+		$("#loadMore").bind('click', function() {
+		
+			results = [];
+		
+			if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight){
+
+				$.getJSON("json/json.html",
+					function(data) {
+						$.each(data, function(index, value) {
+							results.push("<div class='row'>"+value.name+"</div>");
+							})
+						$("#returnData").append(results.join(''));
+						}
+					);
+					
+				}
+			});
+		
+		}
+		
+	else {	
+				
+		$(this).bind('scroll', function() {
+		
+			results = [];
+		
+			if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight){
+
+				$.getJSON("json/json.html",
+					function(data) {
+						$.each(data, function(index, value) {
+							results.push("<div class='row'>"+value.name+"</div>");
+							})
+						$("#returnData").append(results.join(''));
+						}
+					);
+					
+				}
+			});
+			
+		}
+
+	}
